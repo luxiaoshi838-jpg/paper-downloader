417933f297ca8627c8724a6d4b419812046a579f
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-07-27 10:53:25 +0800

    移除未采用的临时入口

--- app_v2.py
+++
@@ -1,8 +0,0 @@
-import app as legacy
-from resolver_v2 import OpenAccessResolverV2
-
-legacy.APP_VERSION = "1.1.0"
-legacy.OpenAccessResolver = OpenAccessResolverV2
-
-if __name__ == "__main__":
-    legacy.main()
